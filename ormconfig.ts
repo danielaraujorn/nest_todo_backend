eac1a91046995ab14466919bbf01f7957c58cf13
Daniel Araújo <daniel.araujorn01@gmail.com>
2020-03-19 21:32:54 -0300

    prepared for production

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,19 +6,23 @@ const TypeOrmConfig = () => {
     DB_PORT,
     DB_NAME,
     DATABASE_URL,
+    NODE_ENV,
   } = process.env
 
   const migrationsDir = '/db/migrations'
-
   const url =
     DATABASE_URL ||
     `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
-  console.log(url)
+
+  const entities =
+    NODE_ENV === 'production'
+      ? ['dist/**/*.entity.js']
+      : [__dirname + '/**/*.entity.ts']
 
   return {
     type: 'postgres',
     url,
-    entities: [__dirname + '/**/*.entity.{ts,js}'],
+    entities,
     migrations: [migrationsDir + '/*.js'],
     cli: { migrationsDir },
     synchronize: true,
